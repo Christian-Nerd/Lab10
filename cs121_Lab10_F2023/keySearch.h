6edc0c12e43f2387124e16190e30ca560064285a
Trey Davis <treydavis@SoundMachine-MBP.local>
2023-11-21 02:10:35 -0500

    Need to add add client.cpp file and change header file. I added a header file and documented it

---
+++ cs121_Lab10_F2023/keySearch.h
@@ -0,0 +1,65 @@
+//*****************************************************************
+//  Programmer: Trey Davis
+//  Instructor: B.J Streller
+//  Course: CS121
+//  Date Created: 11/21/23
+//  Input Files: keys.txt and values.txt 
+//  Output Files: None
+//  Date Modified: 11/21/23
+//  Purpose: Find the index of each element of a set of intger keys in a value set.
+//****************************************************************
+#ifndef keySearch.h
+#define
+#include<fstream>  // For file stream manipulation
+#include<iostream> // For cin & cout
+#include<stdlib.h> // Streller you said always include this
+using namespace std;
+
+//****************************************************************
+// Function Name: SortArray
+// Purpose: Sort array in ascenstion using selection sort
+// Parameters: 
+//     Input: Integer unsorted array 
+//     Input & Output: None
+//     Output: None
+// Return Value: None
+// Non-local variables used: 
+// Functions Called: std::swap
+//****************************************************************
+void SortArray(int[] Value)
+{
+    
+}
+
+//****************************************************************
+// Function Name: GetKeyFile
+// Parameters: None
+// Return Value: File name of keyfile
+// Non-local variables used: None lol
+// Functions Called: none
+//****************************************************************
+string GetKeyFile()
+{
+    //Initializes Variables
+    string KeyFileName; // It's namesake
+    cin >> KeyFileName;
+    //Return value gotten
+    return KeyFileName;
+}
+
+//****************************************************************
+// Function Name: GetKeyFile
+// Parameters: None
+// Return Value: File name of keyfile
+// Non-local variables used: None lol
+// Functions Called: none
+//****************************************************************
+string GetValueFile()
+{
+    //Initializes Variables
+    string KeyFileName; // It's namesake
+    cin >> KeyFileName;
+    //Return value gotten
+    return KeyFileName;
+}
+
---
+++ cs121_Lab10_F2023/keySearchDriver.cpp
@@ -0,0 +1,16 @@
+//*****************************************************************
+//  Programmer: Trey Davis
+//  Instructor: B.J Streller
+//  Course: CS121
+//  Date Created: 11/21/23
+//  Input Files: keys.txt and values.txt 
+//  Output Files: None
+//  Date Modified: 11/21/23
+//  Purpose: Find the index of each element of a set of keys in a value.
+//****************************************************************
+
+int main()
+{
+
+    return 0;
+}
